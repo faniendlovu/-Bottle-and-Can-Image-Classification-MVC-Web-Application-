59a708e53bb696d753401ee78fd5a11e71e49aba
faniendlovu <faniendlovu94@gmail.com>
2023-11-24 21:50:10 +0200

    I removed the API and URL

--- ItuSmartBin/Controllers/HomeController.cs
+++ ItuSmartBin/Controllers/HomeController.cs
@@ -16,45 +16,25 @@ namespace ItuSmartBin.Controllers
     public class HomeController : Controller
     {
 
-        private static string trainingEndpoint = "https://itusmartbin.cognitiveservices.azure.com/";
-        private static string trainingKey = "490b907cee58498a9609dbf42e02a98c";
+        private static string trainingEndpoint = "[URL]";
+        private static string trainingKey = "[API-KEY]";
         // You can obtain these values from the Keys and Endpoint page for your Custom Vision Prediction resource in the Azure Portal.
-        private static string predictionEndpoint = "https://itusmartbin-prediction.cognitiveservices.azure.com/";
-        private static string predictionKey = "9244f9cabaa64daca1541c774d2af877";
+        private static string predictionEndpoint = "[URL]";
+        private static string predictionKey = "[API-KEY]";
 
         //You can obtain this value from the Properties page for your Custom Vision Prediction resource in the Azure Portal.See the "Resource ID" field.This typically has a value such as:
         // /subscriptions/<your subscription ID>/resourceGroups/<your resource group>/providers/Microsoft.CognitiveServices/accounts/<your Custom Vision prediction resource name>
-        private static string predictionResourceId = "/subscriptions/f20f7988-4432-4e11-b635-e7041286715a/resourceGroups/Intelligence/providers/Microsoft.CognitiveServices/accounts/itusmartbin-Prediction";
+        private static string predictionResourceId = "";
 
         private static List<string> hemlockImages;
         private static List<string> japaneseCherryImages;
         private static Tag hemlockTag;
         private static Tag japaneseCherryTag;
         private static Iteration iteration;
-        private static string publishedModelName = "Iteration3";
+        private static string publishedModelName = "Iteration";
         private static MemoryStream testImage;
 
-        // <snippet_creds>
-        //// You can obtain these values from the Keys and Endpoint page for your Custom Vision resource in the Azure Portal.
-        //private static string trainingEndpoint = "https://imageclassification5.cognitiveservices.azure.com/";
-        //private static string trainingKey = "201f527e97524a0b8dcfb20bcd0b560e";
-        //// You can obtain these values from the Keys and Endpoint page for your Custom Vision Prediction resource in the Azure Portal.
-        //private static string predictionEndpoint = "https://imageclassification5-prediction.cognitiveservices.azure.com/";
-        //private static string predictionKey = "e7ec5b2f4b2d4090bcbea6f3325e955f";
-
-        //// You can obtain this value from the Properties page for your Custom Vision Prediction resource in the Azure Portal. See the "Resource ID" field. This typically has a value such as:
-        //// /subscriptions/<your subscription ID>/resourceGroups/<your resource group>/providers/Microsoft.CognitiveServices/accounts/<your Custom Vision prediction resource name>
-        //private static string predictionResourceId = "/subscriptions/f20f7988-4432-4e11-b635-e7041286715a/resourceGroups/Intelligence/providers/Microsoft.CognitiveServices/accounts/ImageClassification5-Prediction";
-
-        //private static List<string> hemlockImages;
-        //private static List<string> japaneseCherryImages;
-        //private static Tag hemlockTag;
-        //private static Tag japaneseCherryTag;
-        //private static Iteration iteration;
-        //private static string publishedModelName = "Iteration1";
-        //private static MemoryStream testImage;
-        //// </snippet_creds>
-
+  
         public ActionResult Index()
         {
             return View();
@@ -91,14 +71,7 @@ namespace ItuSmartBin.Controllers
                 binimageclassificationitem.Tagname = c.TagName;
                 binimageclassificationitem.percentage = c.Probability.ToString("P", CultureInfo.InvariantCulture);
 
-                //if (c.Probability.ToString("P", CultureInfo.InvariantCulture) == "0")
-                //{
-                //    binimageclassificationitem.results = "No";
-                //}
-                //if (c.Probability.ToString("P", CultureInfo.InvariantCulture) == "70")
-                //{
-                //    binimageclassificationitem.results = "Yes";
-                //}
+             
                 binimageclassificationList.Add(binimageclassificationitem);
 
              
